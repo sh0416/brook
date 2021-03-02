1869d226916c400b3b408899b769c5fd06878ad0
Seonghyeon <seonghyeon.drew@gmail.com>
2021-03-02 22:55:19 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name="brook",
+  version="0.0.1",
+  description="Machine learning library",
+  author="Seonghyeon Lee",
+  author_email="seonghyeon.drew@gmail.com",
+  url="https://github.com/sh0416",
+  py_modules=["brook"],
+)
